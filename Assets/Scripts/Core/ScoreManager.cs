c083bab3b424600a8ae1b795f395991fbe73986b
VsPot07 <cocoacake84@gmail.com>
2023-10-27 20:41:37 +0900

    1027

--- Assets/Scripts/Core/ScoreManager.cs
+++ Assets/Scripts/Core/ScoreManager.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using DG.Tweening;
 
 public class ScoreManager : MonoBehaviour
 {
     public static ScoreManager Instance;
     private int _score;
+    [SerializeField] private Canvas _can;
+    [SerializeField] private TextMeshProUGUI _scoreText;
+    [SerializeField] private List<Sprite> _emoziList = new List<Sprite>();
+
+    private Camera _mainCam;
 
     private void Awake()
     {
@@ -15,11 +23,32 @@ public class ScoreManager : MonoBehaviour
             return;
         }
         Instance = this;
+        _mainCam = Camera.main;
     }
 
     public void AddScore(int value)
     {
         _score += value;
         UIManager.Instance.SetScore(_score);
+    }
+
+    public void LookToAddScore(int value, Vector3 pos)
+    {
+        TextMeshProUGUI textpa = Instantiate(_scoreText, _can.transform);
+        textpa.transform.position = _mainCam.WorldToScreenPoint(pos);
+        textpa.text = $"+{value}";
+        Image img = textpa.transform.Find("Emozi").GetComponent<Image>();
+        img.sprite = _emoziList[Random.Range(0, _emoziList.Count)];
+
+        Sequence seq = DOTween.Sequence();
+        float targetv = textpa.transform.position.y + 100;
+        seq.Append(textpa.transform.DOMoveY(targetv, 1f));
+        seq.Append(textpa.DOFade(0, 0.5f));
+        seq.Join(img.DOFade(0, 0.5f));
+
+        seq.AppendCallback(() =>
+        {
+            Destroy(textpa.gameObject);
+        });
     }
 }
--- Assets/Scripts/Func/Board/BoardFunc.cs
+++ Assets/Scripts/Func/Board/BoardFunc.cs
@@ -8,6 +8,7 @@ public class BoardFunc : MonoBehaviour
     private Camera _mainCam;
     [SerializeField] private LayerMask _roadMask;
     [SerializeField] private CinemachineImpulseSource cis;
+    [SerializeField] private float _limitZ;
     private Vector3 dir;
 
     private bool _isRoll = false;
@@ -31,7 +32,7 @@ public class BoardFunc : MonoBehaviour
         if (Input.GetMouseButton(0) && Physics.Raycast(ray, out hit, 100, _roadMask))
         {
             Vector3 pos = new Vector3(transform.position.x, transform.position.y, hit.point.z);
-            if (pos.z <= -3.6f || pos.z >= 3.6f)
+            if (pos.z <= -_limitZ || pos.z >= _limitZ)
             {
                 pos = transform.position;
             }
@@ -40,12 +41,13 @@ public class BoardFunc : MonoBehaviour
         }
     }
 
-    private void Rollick()
+    private void Rollick(Vector3 pos)
     {
         isHit = true;
         if (dir.z == 0) return;
         cis.GenerateImpulse();
         ScoreManager.Instance.AddScore(5000);
+        ScoreManager.Instance.LookToAddScore(5000, pos);
         _isRoll = true;
     }
 
@@ -56,15 +58,16 @@ public class BoardFunc : MonoBehaviour
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.CompareTag("Obstacle") && !_isRoll)
+        if (other.CompareTag("Obstacle") && !_isRoll && !CompareTag("Range"))
         {
-            Rollick();
+            Vector3 pos = other.ClosestPointOnBounds(transform.position);
+            Rollick(pos);
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Obstacle") && _isRoll)
+        if (other.CompareTag("Obstacle") && _isRoll && !CompareTag("Range"))
         {
             _isRoll = false;
             isHit = false;
